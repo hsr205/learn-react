429a0e496e08ae0c1cbdaf4bb8ba41d776dfdffd
Henry Rothenberg <hsr205@aol.com>
2025-01-21 19:25:49 -0500

    Add interface

---
+++ src/types/scientist-profile.ts
@@ -0,0 +1,4 @@
+export interface ScientistProfile {
+    person: { scientistName: string, imageSrc: string },
+    unOrderList: { professionStr: string, awardNum: number, awardDescription: string, scientificDiscovery: string }
+}
