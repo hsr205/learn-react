09af8730315335d45db982fea91cb7c0dca4ecfd
Henry Rothenberg <hsr205@aol.com>
2025-01-21 19:38:59 -0500

    Add item logic

--- src/pages/qcomps/props_item.tsx
+++ src/pages/qcomps/props_item.tsx
@@ -1,31 +1,36 @@
-import { ItemProps } from "@/types/item";
+import {ItemProps} from "@/types/item";
 
-function Item({ name, isPacked } : ItemProps) {
-  return (
-    <li className="item">
-      {name}
-    </li>
-  );
+function Item({name, isPacked}: ItemProps) {
+
+    return (
+        <li className="item">
+            {isPacked ? (
+                    <del>{name}</del>
+                ) :
+                name
+            }
+        </li>
+    );
 }
 
 export default function PackingList() {
-  return (
-    <section>
-      <h1>My Packing List</h1>
-      <ul>
-        <Item
-          isPacked={true}
-          name="Laptop"
-        />
-        <Item
-          isPacked={true}
-          name="Chargers"
-        />
-        <Item
-          isPacked={false}
-          name="Socks"
-        />
-      </ul>
-    </section>
-  );
+    return (
+        <section>
+            <h1>My Packing List</h1>
+            <ul>
+                <Item
+                    isPacked={true}
+                    name="Laptop"
+                />
+                <Item
+                    isPacked={true}
+                    name="Chargers"
+                />
+                <Item
+                    isPacked={false}
+                    name="Socks"
+                />
+            </ul>
+        </section>
+    );
 }
