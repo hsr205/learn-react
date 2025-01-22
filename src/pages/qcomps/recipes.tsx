b36f62f90f86f6ab2e52b106fd87dc6dfd14e4ff
Henry Rothenberg <hsr205@aol.com>
2025-01-21 20:33:19 -0500

    Alter react component logic

--- src/pages/qcomps/recipes.tsx
+++ src/pages/qcomps/recipes.tsx
@@ -1,22 +1,35 @@
 export const recipes = [{
-  id: 'greek-salad',
-  name: 'Greek Salad',
-  ingredients: new Set(['tomatoes', 'cucumber', 'onion', 'olives', 'feta'])
+    id: 'greek-salad',
+    name: 'Greek Salad',
+    ingredients: new Set(['tomatoes', 'cucumber', 'onion', 'olives', 'feta'])
 }, {
-  id: 'hawaiian-pizza',
-  name: 'Hawaiian Pizza',
-  ingredients: new Set(['pizza crust', 'pizza sauce', 'mozzarella', 'ham', 'pineapple'])
+    id: 'hawaiian-pizza',
+    name: 'Hawaiian Pizza',
+    ingredients: new Set(['pizza crust', 'pizza sauce', 'mozzarella', 'ham', 'pineapple'])
 }, {
-  id: 'hummus',
-  name: 'Hummus',
-  ingredients: new Set(['chickpeas', 'olive oil', 'garlic cloves', 'lemon', 'tahini'])
+    id: 'hummus',
+    name: 'Hummus',
+    ingredients: new Set(['chickpeas', 'olive oil', 'garlic cloves', 'lemon', 'tahini'])
 }];
 
 export default function RecipeList() {
-  return (
-    <div>
-      <h1>Recipes</h1>
-      {}
-    </div>
-  );
+    return (
+        <div>
+            <h1>Recipes</h1>
+
+
+            {recipes.map((recipe) => (
+                <ul key={recipe.id}>
+                    <li>{recipe.name}</li>
+                    {Array.from(recipe.ingredients).map((ingredient, index) => (
+                        <li key={index}>
+                            {ingredient}
+                        </li>
+                    ))}
+                </ul>
+            ))}
+
+
+        </div>
+    );
 }
