39ac0196db1b4e40b7c0279d0a4b05e1e37a8c34
Henry Rothenberg <hsr205@aol.com>
2025-01-29 07:49:06 -0500

    Fix bug

--- src/pages/components/state.tsx
+++ src/pages/components/state.tsx
@@ -5,7 +5,7 @@ export default function Gallery() {
     const [index, setIndex] = useState(0);
 
     function handleClick() {
-        setIndex(index + 1);
+        setIndex((previousIndex) => (previousIndex + 1) % sculptureList.length);
     }
 
     let sculpture = sculptureList[index];
