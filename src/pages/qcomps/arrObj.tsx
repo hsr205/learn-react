bb52942dd91c199e1ba5128c82945041a528c55e
Henry Rothenberg <hsr205@aol.com>
2025-01-29 08:57:30 -0500

    Fix bug

--- src/pages/qcomps/arrObj.tsx
+++ src/pages/qcomps/arrObj.tsx
@@ -1,74 +1,78 @@
-import { useState } from 'react';
-import { ItemListProps } from '../../types/arrObj';
+import {useState} from 'react';
+import {ItemListProps} from '../../types/arrObj';
 
 let nextId = 3;
-const initialList = [
-  { id: 0, title: 'Big Bellies', seen: false },
-  { id: 1, title: 'Lunar Landscape', seen: false },
-  { id: 2, title: 'Terracotta Army', seen: true },
+const listOne = [
+    {id: 0, title: 'Big Bellies', seen: false},
+    {id: 1, title: 'Lunar Landscape', seen: false},
+    {id: 2, title: 'Terracotta Army', seen: true},
+];
+
+const listTwo = [
+    {id: 0, title: 'Big Bellies', seen: false},
+    {id: 1, title: 'Lunar Landscape', seen: false},
+    {id: 2, title: 'Terracotta Army', seen: true},
 ];
 
 export default function BucketList() {
-  const [myList, setMyList] = useState(initialList);
-  const [yourList, setYourList] = useState(
-    initialList
-  );
+    const [myList, setMyList] = useState(listOne);
+    const [yourList, setYourList] = useState(listTwo);
 
-  function handleToggleMyList(artworkId: number, nextSeen: boolean) {
-    const tmpList = myList.map(e => {
-        if (e.id === artworkId) {
-            e.seen = nextSeen
-        }
-        return e
-    });
-    setMyList(tmpList);
-  }
+    function handleToggleMyList(artworkId: number, nextSeen: boolean) {
+        const tmpList = myList.map(e => {
+            if (e.id === artworkId) {
+                e.seen = nextSeen
+            }
+            return e
+        });
+        setMyList(tmpList);
+    }
 
-  function handleToggleYourList(artworkId: number, nextSeen: boolean) {
-    const tmpList = yourList.map(e => {
-        if (e.id === artworkId) {
-            e.seen = nextSeen
-        }
-        return e
-    });
-    setYourList(tmpList);
-  }
+    function handleToggleYourList(artworkId: number, nextSeen: boolean) {
+        const tmpList = yourList.map(e => {
+            if (e.id === artworkId) {
+                e.seen = nextSeen
+            }
+            return e
+        });
+        setYourList(tmpList);
+    }
 
-  return (
-    <>
-      <h1>Art Bucket List</h1>
-      <h2>My list of art to see:</h2>
-      <ItemList
-        artworks={myList}
-        onToggle={handleToggleMyList} />
-      <h2>Your list of art to see:</h2>
-      <ItemList
-        artworks={yourList}
-        onToggle={handleToggleYourList} />
-    </>
-  );
+    return (
+        <>
+            <h1>Art Bucket List</h1>
+            <h2>My list of art to see:</h2>
+            <ItemList
+                artworks={myList}
+                onToggle={handleToggleMyList}/>
+            <h2>Your list of art to see:</h2>
+            <ItemList
+                artworks={yourList}
+                onToggle={handleToggleYourList}/>
+        </>
+    );
 }
 
-function ItemList({ artworks, onToggle }: ItemListProps) {
-  return (
-    <ul>
-      {artworks.map(artwork => (
-        <li key={artwork.id}>
-          <label>
-            <input
-              type="checkbox"
-              checked={artwork.seen}
-              onChange={e => {
-                onToggle(
-                  artwork.id,
-                  e.target.checked
-                );
-              }}
-            />
-            {artwork.title}
-          </label>
-        </li>
-      ))}
-    </ul>
-  );
+function ItemList({artworks, onToggle}: ItemListProps) {
+    return (
+        <ul>
+            {artworks.map(artwork => (
+                <li key={artwork.id}>
+                    <label>
+                        <input
+                            type="checkbox"
+                            checked={artwork.seen}
+                            onChange={e => {
+                                onToggle(
+                                    artwork.id,
+                                    e.target.checked
+                                );
+                            }}
+                        />
+                        {artwork.title}
+                    </label>
+                </li>
+            ))}
+        </ul>
+    );
 }
