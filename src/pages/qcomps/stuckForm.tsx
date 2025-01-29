d13dd40f9e3a4048d5fe42951ad0bb0b1594c568
Henry Rothenberg <hsr205@aol.com>
2025-01-29 08:10:24 -0500

    Fix bug

--- src/pages/qcomps/stuckForm.tsx
+++ src/pages/qcomps/stuckForm.tsx
@@ -1,34 +1,29 @@
+import {useState} from "react";
+
 export default function Form() {
-  let firstName = '';
-  let lastName = '';
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
 
-  function handleFirstNameChange(e: { target: { value: string; }; }) {
-    firstName = e.target.value;
-  }
+    function handleReset() {
+        setFirstName('')
+        setLastName('')
+    }
 
-  function handleLastNameChange(e: { target: { value: string; }; }) {
-    lastName = e.target.value;
-  }
 
-  function handleReset() {
-    firstName = '';
-    lastName = '';
-  }
-
-  return (
-    <form onSubmit={e => e.preventDefault()}>
-      <input
-        placeholder="First name"
-        value={firstName}
-        onChange={handleFirstNameChange}
-      />
-      <input
-        placeholder="Last name"
-        value={lastName}
-        onChange={handleLastNameChange}
-      />
-      <h1>Hi, {firstName} {lastName}</h1>
-      <button onClick={handleReset}>Reset</button>
-    </form>
-  );
+    return (
+        <form onSubmit={e => e.preventDefault()}>
+            <input
+                placeholder="First name"
+                value={firstName}
+                onChange={event => setFirstName(event.target.value)}
+            />
+            <input
+                placeholder="Last name"
+                value={lastName}
+                onChange={event => setLastName(event.target.value)}
+            />
+            <h1>Hi, {firstName} {lastName}</h1>
+            <button onClick={handleReset}>Reset</button>
+        </form>
+    );
 }
