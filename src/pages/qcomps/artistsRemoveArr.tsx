238372cb741e1b325784be6861ee951f2483cdf1
Henry Rothenberg <hsr205@aol.com>
2025-01-29 08:53:35 -0500

    Fix bug

--- src/pages/qcomps/artistsRemoveArr.tsx
+++ src/pages/qcomps/artistsRemoveArr.tsx
@@ -1,31 +1,32 @@
-import { useState } from 'react';
+import {useState} from 'react';
 
 let initialArtists = [
-  { id: 0, name: 'Marta Colvin Andrade' },
-  { id: 1, name: 'Lamidi Olonade Fakeye'},
-  { id: 2, name: 'Louise Nevelson'},
+    {id: 0, name: 'Marta Colvin Andrade'},
+    {id: 1, name: 'Lamidi Olonade Fakeye'},
+    {id: 2, name: 'Louise Nevelson'},
 ];
 
 export default function List() {
-  const [artists, setArtists] = useState(
-    initialArtists
-  );
+    const [artists, setArtists] = useState(
+        initialArtists
+    );
 
-  return (
-    <>
-      <h1>Inspiring sculptors:</h1>
-      <ul>
-        {artists.map(artist => (
-          <li key={artist.id}>
-            {artist.name}{' '}
-            <button onClick={() => {
-              artists.splice(artist.id, 1)
-            }}>
-              Delete
-            </button>
-          </li>
-        ))}
-      </ul>
-    </>
-  );
+    return (
+        <>
+            <h1>Inspiring sculptors:</h1>
+            <ul>
+                {artists.map(artist => (
+                    <li key={artist.id}>
+                        {artist.name}{' '}
+                        <button onClick={() => {
+                            //   TODO: Should not use splice, use .filter() or .slice() instead
+                            setArtists([...artists.filter(artistId => artistId.id !== artist.id)]);
+                        }}>
+                            Delete
+                        </button>
+                    </li>
+                ))}
+            </ul>
+        </>
+    );
 }
